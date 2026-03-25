856ef7bff49fd8a616d419895506cfe8a919cd7e
Neil Masion <neilmasion2@example.com>
2026-03-25 23:42:02 +0800

    Render: avoid manifest static missing font failure

--- flurry_project/settings.py
+++ flurry_project/settings.py
@@ -106,7 +106,7 @@ STATICFILES_DIRS = [
 ]
 STORAGES = {
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     }
 }
 
