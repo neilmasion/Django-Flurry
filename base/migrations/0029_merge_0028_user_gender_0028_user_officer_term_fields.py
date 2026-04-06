576008b0f8f8af1020a23ef9c9e9e5b2b6ccbad6
Neil Masion <neilmasion2@example.com>
2026-04-06 20:22:20 +0800

    Minor Fix: making migrations

---
+++ base/migrations/0029_merge_0028_user_gender_0028_user_officer_term_fields.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0028_user_gender'),
+        ('base', '0028_user_officer_term_fields'),
+    ]
+
+    operations = [
+    ]
